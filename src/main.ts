79d6e309be01d648f87bcd44bc90871cdbd47edd
edkiada <yyh901277111@gmail.com>
2026-04-02 23:40:23 +0800

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import '../src/styles/style.css'
-import router from './router/index'
+import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
